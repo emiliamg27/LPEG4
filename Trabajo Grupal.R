4dd6a320b03a415b40bbb96b6aa5462ca0192c28
Marta Díaz Fuentes <martadiazfuentes@outlook.es>
2022-11-30 12:04:31 +0100

    Anadimos columnas low y ccaa

--- Trabajo Grupal.R
+++ Trabajo Grupal.R
@@ -3,36 +3,6 @@
 ## LENGUAJES DE PROGRAMACIÓN ESTADÍSTICA
 ## PROFESOR: CHRISTIAN SUCUZHANAY AREVALO
 
-# SHORTCUTS ---------------------------------------------------------
-## ctrl + l = clean console
-## ctrl + shift + r = new section
-
-# GIT COMMANDS ------------------------------------------------------------
-# git status
-# git init 
-# git add
-# git commit -m "message"
-# git push -u origin main 
-# git branch Emilia
-# git merge
-# git remote add origin https://github.com/martadzf/LPE21535220.git
-# git clone https://github.com/martadzf/LPE21535220.git
-# git pull
-# git fetch
-
-# TERMINAL COMANDS --------------------------------------------------------
-# ls listar 
-# cd meterte en sitios
-# pwd para ver donde estas
-# cd .. volver atras 
-# mkdir crear directorio
-# touch crear archivo
-# nano
-# less abrir archivos
-# cat abrir archivo moviendo raton
-# clear
-# which
-
 # LOADING LIBS ------------------------------------------------------------
 if(!require("pacman")) install.packages("pacman")
 p_load(tidyverse, magrittr, janitor, lubridate, tidyr, httr)
@@ -49,24 +19,26 @@ janitor::clean_names(df_source) %>% glimpse()
 locale() 
 #Cambiamos config
 df1<-df_source %>% janitor::clean_names() %>% type_convert(locale = locale(decimal_mark = ","))
-str_squish(df1)
+str_squish(df1$rotulo)
 
 # CREAR NUEVAS VARIABLES ------------------------------------------------
 
 ##Clasificamos por gasolineras baratas y no baratas NUEVA COLUMNA
 df2 <- df1 %>% mutate(lowcost =! rotulo %in% c("CEPSA", "REPSOL", "BP", "SHELL"))
 
+##Columna nombre ccaa
+df3 <- df2%>% mutate(df2,ccaa = ifelse (idccaa=="01","ANDALUCIA",ifelse (idccaa=="02","ARAGON", ifelse (idccaa=="03","ASTURIAS", ifelse (idccaa=="04","BALEARES", 
+                                                                                                                                                ifelse (idccaa=="05","CANARIAS",ifelse (idccaa=="06","CANTABRIA", ifelse (idccaa=="07","CASTILLA Y LEON", 
+                                                                                                                                                                                                                          ifelse (idccaa=="08","CASTILLA - LA MANCHA", ifelse (idccaa=="09","CATALUÑA", ifelse (idccaa=="10","COMUNIDAD VALENCIANA",
+                                                                                                                                                                                                                                                                                                                ifelse (idccaa=="11","EXTREMADURA", ifelse (idccaa=="12","GALICIA", ifelse (idccaa=="13","MADRID", ifelse (idccaa=="14","MURCIA", ifelse (idccaa=="15","NAVARRA",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                          ifelse (idccaa=="16","PAIS VASCO", ifelse (idccaa=="17","LA RIOJA",ifelse (idccaa=="18","CEUTA",ifelse (idccaa=="19","MELILLA","NA"))))))))))))))))))))
 ##Precio medio del gasoleo en las CCAA
 dflow %>% select(precio_gasoleo_a, idccaa, rotulo, lowcost) %>% drop_na() %>% group_by(idccaa, lowcost) %>% summarize(mean(precio_gasoleo_a)) %>% view()
 
 
 write.csv(dflow,"ds21903993(33).csv")
 
-##Columna nombre ccaa
-dfccaa <- dflow%>% mutate(dflow,ccaa = ifelse (idccaa=="01","ANDALUCIA",ifelse (idccaa=="02","ARAGON", ifelse (idccaa=="03","ASTURIAS", ifelse (idccaa=="04","BALEARES", 
-                                                                                          ifelse (idccaa=="05","CANARIAS",ifelse (idccaa=="06","CANTABRIA", ifelse (idccaa=="07","CASTILLA Y LEON", 
-                                                                                                                                                                    ifelse (idccaa=="08","CASTILLA - LA MANCHA", ifelse (idccaa=="09","CATALUÑA", ifelse (idccaa=="10","COMUNIDAD VALENCIANA",                                                                                                                                                                                                                                                          ifelse (idccaa=="11","EXTREMADURA", ifelse (idccaa=="12","GALICIA", ifelse (idccaa=="13","MADRID", ifelse (idccaa=="14","MURCIA", ifelse (idccaa=="15","NAVARRA",
-                                                                                                                                                                                                                                                                  ifelse (idccaa=="16","PAIS VASCO", ifelse (idccaa=="17","LA RIOJA",ifelse (idccaa=="18","CEUTA",ifelse (idccaa=="19","MELILLA","NA")))))))))))))))))))) %>% view()
+
 write.csv(dfccaa,"ds21903993(34).csv")
 ##Gasolinera más barata
   
